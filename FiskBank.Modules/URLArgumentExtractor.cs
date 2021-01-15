4a6075f2d7cf092e7f8dbb8880b32b2ee68bbf22
Jonathas Castilho <jonathascatilhoes@gmail.com>
2021-01-15 15:25:03 -0300

    Adding a new Class to fetch for arguments in an url

---
+++ FiskBank.Modules/URLArgumentExtractor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiskBank.Modules
+{
+    public class URLArgumentExtractor
+    {
+        private readonly string _url;
+        private readonly int _index;
+
+        //fiskbank?fromCurrency=bit&toCurrency=fiskdollar
+        public URLArgumentExtractor(string url)
+        {
+            String.IsNullOrEmpty(url);
+            _index = url.IndexOf('?');
+            _url = url.Substring(_index + 1);
+        }
+
+        /// <summary>
+        /// Returns argument especified on instanced object.
+        /// </summary>
+        /// <param name="paramName">Parameter's name to be extracted by the method.</param>
+        /// <returns></returns>
+        public string GetArgument(string paramName)
+        {
+            string urlUpper = _url.ToUpper();
+
+            string parameter = (paramName + "=").ToUpper();
+            int argumentIndex = urlUpper.IndexOf(parameter) + parameter.Length;
+
+            string argumentTrimmed = _url.Substring(argumentIndex);
+            int removeIndex = argumentTrimmed.IndexOf('&');
+
+            if (removeIndex == -1) return argumentTrimmed;
+
+            return argumentTrimmed.Remove(removeIndex);
+        }
+    }
+}
--- FiskBank/Program.cs
+++ FiskBank/Program.cs
@@ -16,7 +16,15 @@ namespace FiskBank
         public static int operation;
         static void Main(string[] args)
         {
-            ToCreateAccount();
+            URLArgumentExtractor url = new URLArgumentExtractor("fiskbank?fromCurrency=bit&toCurrency=fiskDollar&value=100");
+            Console.WriteLine(url.GetArgument("fromCurrency"));
+            Console.WriteLine(url.GetArgument("toCurrency"));
+            Console.WriteLine(url.GetArgument("value"));
+            Console.WriteLine(url.GetArgument("FROMcurrency"));
+            Console.WriteLine(url.GetArgument("TOcurRENCY"));
+            Console.WriteLine(url.GetArgument("value"));
+            
+
             Console.ReadLine();
         }
 
